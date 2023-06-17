1fa50f27cec9cb92bccc846496dd13fbbf22885f
Kerem Okumus <96080839+Keremovsky@users.noreply.github.com>
2023-06-17 17:25:02 +0300

    ui colors added

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -12,70 +12,74 @@ class _homePageState extends State<loginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body:Container(
-        child: Column(
-        mainAxisAlignment:
-          MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-          const Text(
-          "App Name",
-          style: TextStyle(
-          color: Colors.black,
-          fontSize: 25,
-          ),
-          ),
-          const SizedBox(
-            height: 44.0,
+      body: Container(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+            const Text(
+              "App Name",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+              ),
             ),
-          Container(
-            width: double.infinity,
-            child: RawMaterialButton(
-            fillColor: Color.fromARGB(255, 33, 150, 243),
-            elevation: 0.0,
-            padding: EdgeInsets.symmetric (vertical: 15.0),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0)),
-              onPressed: () {},
-            child: Text("Login with Google",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 19,
-            ),)
+            const SizedBox(
+              height: 44.0,
             ),
+            Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                  fillColor: Color.fromARGB(255, 33, 150, 243),
+                  elevation: 0.0,
+                  padding: EdgeInsets.symmetric(vertical: 15.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  onPressed: () {},
+                  child: Text(
+                    "Login with Google",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 19,
+                    ),
+                  )),
             ),
             SizedBox(
-              height: 12.0, ),
-          const SizedBox(
-            height: 20.0, ),
-          Container(
-            width: double.infinity,
-            child: RawMaterialButton(
-            fillColor: Color.fromARGB(255, 33, 150, 243),
-            elevation: 0.0,
-            padding: EdgeInsets.symmetric (vertical: 15.0),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0)),
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return Scaffold(
-                          appBar: AppBar(),
-                          body:loginMailScreen()
-                        );
-              },),);},
-            child: Text("Login with E-mail",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18.8,
-            ),)
-            ),),
+              height: 12.0,
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                  fillColor: Color.fromARGB(255, 33, 150, 243),
+                  elevation: 0.0,
+                  padding: EdgeInsets.symmetric(vertical: 15.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return Scaffold(
+                              appBar: AppBar(), body: loginMailScreen());
+                        },
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "Login with E-mail",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.8,
+                    ),
+                  )),
+            ),
             SizedBox(
-              height: 12.0, ),
-          ]
-        )
-      ),
+              height: 12.0,
+            ),
+          ])),
     );
   }
 }
@@ -91,70 +95,77 @@ class _loginMailScreen extends State<loginMailScreen> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment:
-          MainAxisAlignment.center,
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-          const Text(
-          "App Name",
-          style: TextStyle(
-          color: Colors.black,
-          fontSize: 25,
-          ),
-          ),
-          const SizedBox(
-            height: 44.0,
+            const Text(
+              "App Name",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+              ),
             ),
-          const TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-            hintText: "Email",
-            prefixIcon: Icon (Icons.mail, color: Colors.grey), ),),
-          const SizedBox(
-            height: 26.0,
-          ),
-          const TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              hintText: "Password",
-              prefixIcon: Icon (Icons.remove_red_eye, color: Colors.grey),
-          )),
-          const SizedBox(
-            height: 12.0, ),
-          const Text(
-          "Forget your password?",
-          style: TextStyle(color: Colors.blue), ),
-          const SizedBox(
-          height: 88.0, ),
-          Container(
-            width: double.infinity,
-            child: RawMaterialButton(
-            fillColor: Color.fromARGB(255, 33, 150, 243),
-            elevation: 0.0,
-            padding: EdgeInsets.symmetric (vertical: 15.0),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0)),
-              onPressed: () {},
-            child: Text("Login",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18.8,
-            ),)
+            const SizedBox(
+              height: 44.0,
             ),
+            const TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "Email",
+                prefixIcon: Icon(Icons.mail, color: Colors.grey),
+              ),
+            ),
+            const SizedBox(
+              height: 26.0,
+            ),
+            const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: Icon(Icons.remove_red_eye, color: Colors.grey),
+                )),
+            const SizedBox(
+              height: 12.0,
+            ),
+            const Text(
+              "Forget your password?",
+              style: TextStyle(color: Colors.blue),
+            ),
+            const SizedBox(
+              height: 88.0,
+            ),
+            Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                  fillColor: Color.fromARGB(255, 33, 150, 243),
+                  elevation: 0.0,
+                  padding: EdgeInsets.symmetric(vertical: 15.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.8,
+                    ),
+                  )),
             ),
             SizedBox(
-              height: 12.0, ),
+              height: 12.0,
+            ),
             Align(
               child: const Text(
-              "Don't have account? Sign Up",
-              style: TextStyle(color: Colors.blue), ),
+                "Don't have account? Sign Up",
+                style: TextStyle(color: Colors.blue),
+              ),
             ),
-          const SizedBox(
-            height: 88.0, )
-            ],
+            const SizedBox(
+              height: 88.0,
             )
-            );
+          ],
+        ));
   }
 }
