0bb9192f279b22b039130713e3db8e8a51835fbb
Kerem Okumus <96080839+Keremovsky@users.noreply.github.com>
2023-06-30 17:06:48 +0300

    config of app

--- lib/features/home/profile_screen.dart
+++ lib/features/home/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:bootcamp_flutter/themes/palette.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -7,8 +6,10 @@ import 'package:image_picker/image_picker.dart';
 import '../../core/constants/constants.dart';
 
 class ProfileScreen extends StatefulWidget {
+  static final routeName = "/profileScreen";
+
   const ProfileScreen({super.key});
-  
+
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
@@ -35,109 +36,103 @@ class _ProfileScreenState extends State<ProfileScreen> {
       ),
       body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              height: 50,
-              color: Palette.buttonBackground,
-            ),
-            Stack(
-              children: [Container(
-                width: double.infinity,
-                height: 110,
-                decoration: BoxDecoration(
-                  color: Palette.buttonBackground,
-                    )
-                ),
-            Center(
-                child: Container(
-                  height: 220,
-                  width: 220,
-                  child: InkWell(
-                    onTap: _pickImage,
-                    child: CircleAvatar(
-                      foregroundImage: _image != null ? FileImage(_image!) : null,
-                      backgroundImage: const AssetImage(Constants.profilePicture),
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 50,
+                color: Palette.buttonBackground,
+              ),
+              Stack(
+                children: [
+                  Container(
+                      width: double.infinity,
+                      height: 110,
+                      decoration: BoxDecoration(
+                        color: Palette.buttonBackground,
+                      )),
+                  Center(
+                    child: Container(
+                      height: 220,
+                      width: 220,
+                      child: InkWell(
+                        onTap: _pickImage,
+                        child: CircleAvatar(
+                          foregroundImage:
+                              _image != null ? FileImage(_image!) : null,
+                          backgroundImage:
+                              const AssetImage(Constants.profilePicture),
+                        ),
+                      ),
                     ),
                   ),
+                ],
+              ),
+              const SizedBox(height: 80),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                child: Container(
+                  width: double.infinity,
+                  child: RawMaterialButton(
+                      fillColor: Palette.textFieldBackground,
+                      elevation: 0.0,
+                      padding: const EdgeInsets.symmetric(vertical: 15.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      onPressed: () {},
+                      child: Text(
+                        "Past Goals",
+                        style: TextStyle(
+                            color: Palette.buttonText,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      )),
                 ),
               ),
-            ],),
-            SizedBox(height: 80,),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-              child: Container(
-                width: double.infinity,
-                child: RawMaterialButton(
-                  fillColor: Palette.textFieldBackground,
-                  elevation: 0.0,
-                  padding: const EdgeInsets.symmetric(vertical: 15.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0)),
-                  onPressed: () {  },
-                  child: Text(
-                    "Past Goals",
-                    style: TextStyle(
-                      color: Palette.buttonText,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold
-                      ),
-                    )
+              const SizedBox(height: 35),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                child: Container(
+                  width: double.infinity,
+                  child: RawMaterialButton(
+                      fillColor: Palette.textFieldBackground,
+                      elevation: 0.0,
+                      padding: const EdgeInsets.symmetric(vertical: 15.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      onPressed: () {},
+                      child: Text(
+                        "Past Actions",
+                        style: TextStyle(
+                            color: Palette.buttonText,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      )),
                 ),
               ),
-            ),
-            SizedBox(height: 35,),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-              child: Container(
-                width: double.infinity,
-                child: RawMaterialButton(
-                  fillColor: Palette.textFieldBackground,
-                  elevation: 0.0,
-                  padding: const EdgeInsets.symmetric(vertical: 15.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0)),
-                  onPressed: () {  },
-                  child: Text(
-                    "Past Actions",
-                    style: TextStyle(
-                      color: Palette.buttonText,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold
-                      ),
-                    )
+              const SizedBox(height: 35),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                child: Container(
+                  width: double.infinity,
+                  child: RawMaterialButton(
+                      fillColor: Palette.textFieldBackground,
+                      elevation: 0.0,
+                      padding: const EdgeInsets.symmetric(vertical: 15.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      onPressed: () {},
+                      child: Text(
+                        "Automated Actions",
+                        style: TextStyle(
+                            color: Palette.buttonText,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      )),
                 ),
               ),
-            ),
-            SizedBox(height: 35,),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-              child: Container(
-                width: double.infinity,
-                child: RawMaterialButton(
-                  fillColor: Palette.textFieldBackground,
-                  elevation: 0.0,
-                  padding: const EdgeInsets.symmetric(vertical: 15.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0)),
-                  onPressed: () {  },
-                  child: Text(
-                    "Automated Actions",
-                    style: TextStyle(
-                      color: Palette.buttonText,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold
-                    ),
-                  )
-                ),
-              ),
-            ),
-          ]
-        ),
+            ]),
       ),
     );
   }
 }
-
-
